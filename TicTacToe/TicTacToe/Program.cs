ec16a84e89e3be3d1a852b1d0a30a87ec06da367
Jason Bell <jbell@accelebrate.com>
2021-03-08 13:59:47 -0800

    Vars

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Text;
 
 namespace TicTacToe
 {
+    enum Piece { X, O }
+
     class Program
     {
         static void Main(string[] args)  
         {
+            Piece? pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9;
+
+            Piece currentPlayer = Piece.X;
+
             Console.WriteLine("   |   |   ");
             Console.WriteLine("---+---+---");
             Console.WriteLine("   |   |   ");
@@ -14,3 +21,8 @@ namespace TicTacToe
         }
     }
 }
+
+// LAB 1
+// 1) Define a type to represent a piece (enum)
+// 2) Define a variable for each position
+// 3) Define a variable for the current player
