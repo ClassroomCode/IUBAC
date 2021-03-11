4df88e627cc04b1740443b741f088c58184a2372
Jason Bell <jbell@accelebrate.com>
2021-03-11 10:57:29 -0800

    Lib

---
+++ OopDemo/OopDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OopDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new List<Person>();
+            var per = new Person() { FirstName = "Bill", LastName = "Gates" };
+            people.Add(per);
+            
+            foreach (var p in people) {
+                Console.WriteLine(p.FullName);
+            }
+        }
+    }
+}
