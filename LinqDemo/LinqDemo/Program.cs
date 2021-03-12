7efb8834bce9e598cd8ea6bf1c532eb92e08c133
Jason Bell <jbell@accelebrate.com>
2021-03-12 09:35:55 -0800

    Linq update

--- LinqDemo/LinqDemo/Program.cs
+++ LinqDemo/LinqDemo/Program.cs
@@ -11,14 +11,14 @@ namespace LinqDemo
             var nums = new List<int>() { 1, 2, 3, 4, 5, 6 };
 
             var evens2 = from n in nums
-                        where n % 2 == 0 && n > 2
-                        orderby n descending
-                        select n;
+                         where n % 2 == 0 && n > 2
+                         orderby n descending
+                         select n;
 
             var evens = nums.Where(n => n % 2 == 0 && n > 2)
                 .OrderByDescending(n => n);
 
             evens.ToList().ForEach(n => Console.WriteLine(n));
         }
-    }
-}
+    } 
+} 
