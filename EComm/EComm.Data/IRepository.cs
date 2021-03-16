46b9620a634451b537ad7bf7d2da966bf2657ed7
Jason Bell <jbell@accelebrate.com>
2021-03-16 09:55:26 -0700

    EComm

---
+++ EComm/EComm.Data/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using EComm.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EComm.Data
+{
+    public interface IRepository
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task<Product> GetProduct(int id);
+    }
+}
